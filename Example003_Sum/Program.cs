9b4093dadf545d1f79e70e86c4de29256094f001
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-15 17:38:36 +0500

    Сhoice one rendom numer in sum added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 31;
+﻿int numberA = new Random().Next(1,10); // Числа 1,2,3,4 ...9
+Console.WriteLine($"Случайное число: {numberA}");
 int numberB = 5;
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine($"Сумма случаного числа и {numberB} = {result}");
