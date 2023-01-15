6e676a90a426ffb9faf4620ebe37a90ec17baad5
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-15 20:06:22 +0500

    Example 010 'Method array' added

---
+++ lecture2/Example010MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+int[] array = { 1, 2, 31, 4, 18, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
