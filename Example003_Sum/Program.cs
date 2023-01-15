8c20c051ec9f685feb3b16520f8df51a6a05cef2
Daniel Kharitonov <Daniel.Kharitonov@gmail.com>
2023-01-15 20:12:03 +0500

    Delete Example003_Sum directory

--- Example003_Sum/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿int numberA = new Random().Next(1,10); // Числа 1,2,3,4 ...9
-Console.WriteLine($"Случайное число: {numberA}");
-int numberB = 5;
-int result = numberA + numberB;
-Console.WriteLine($"Сумма случаного числа и {numberB} = {result}");
