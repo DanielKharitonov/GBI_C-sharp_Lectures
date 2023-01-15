c0f6e9930e1c8cf6da878cb408c652442c2be0dd
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-15 17:21:48 +0500

    File '.gitignore' added

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Enter your name: ");
-string username = Console.ReadLine();
-Console.Write ("Привет, ");
+string username = Console.ReadLine()!;
+Console.Write ("Hello, ");
 Console.Write (username); 
